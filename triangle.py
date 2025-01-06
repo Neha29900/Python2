91288c2f1dd86b9bb176efd1fc68b3a19aff4858
Neha29900 <pneha3836@gmail.com>
2025-01-06 16:55:02 +0530

    C sharp program to print a pyramid using *

--- triangle.py
+++ triangle.py
@@ -1,2 +1,23 @@
-def triangle(t)
-    
+rows = int(input("Enter the number of rows: "))
+
+for i in range(1, rows + 1):
+    print("*" * i)
+
+
+for i in range(1, rows + 1):
+
+   
+    print(" " * (rows - i), end="")
+   
+    print("*" * (2 * i - 1))
+
+
+
+for i in range(1, rows + 1):
+    print(" " * (rows - i), end="")
+    print("*" * (2 * i - 1))
+
+for i in range(rows - 1, 0, -1):
+   
+    print(" " * (rows - i), end="")
+    print("*" * (2 * i - 1))
