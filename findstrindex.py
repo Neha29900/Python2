4e183a87c3728451048beb6d61c875bb132478d3
Neha29900 <153270760+Neha29900@users.noreply.github.com>
2025-01-07 13:07:47 +0530

    Find first occurance index of the string

---
+++ findstrindex.py
@@ -0,0 +1,8 @@
+str = "hello world"
+fstr= "world"
+index = strk.find(fstr)
+
+if index != -1:
+    print(f"The first occurrence of '{fstr}' is at index {index}.")
+else:
+    print(f"'{fstr}' is not found in the string.")
