ebde2e31e1bceed65591adfdc1ea8c7fb1c72bb5
Neha29900 <153270760+Neha29900@users.noreply.github.com>
2025-03-13 13:49:26 +0530

    Conversion From Roman to Integer

--- LengthofLastWord.py
+++ LengthofLastWord.py
@@ -1,1 +1,7 @@
-LengthofLastWord
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.strip().split(" ")[-1])
